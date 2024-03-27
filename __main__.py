9a3f2dffc0851bd07588de107f8cf632934b7e9c
Ian Wahbe <ian@wahbe.com>
2024-03-27 19:37:08 +0100

    Repro https://github.com/pulumi/pulumi-go-provider/issues/204

---
+++ __main__.py
@@ -0,0 +1,19 @@
+import pulumi
+import pinecone_pulumi as pinecone
+
+pinecone_index = pinecone.PineconeIndex(
+    "my-index",
+    name="my-index",
+    spec=pinecone.PineconeSpecArgs(
+        pod=pinecone.PineconePodSpecArgs(
+            environment="gcp-starter",
+            pod_type="starter",
+            replicas=1,
+        ),
+    ),
+    metric="cosine",
+    dimension=1536,
+)
+pulumi.export("output", {
+    "value": pinecone_index.host,
+})
