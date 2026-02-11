d73e5960b07037700f45f4fd7e6578b409dc3094
Ian Wahbe <me@iwahbe.com>
2026-02-11 12:22:10 +0100

    Attempt repro of https://github.com/pulumi/pulumi/issues/21635

--- __main__.py
+++ __main__.py
@@ -1,1 +1,10 @@
-import pulumi_random as random
+import pulumi
+import moolumi_random_component_1 as v1
+import moolumi_random_component_2 as v2
+import moolumi_random_component_3 as v3
+import moolumi_random_component_4 as v4
+
+v1.RandomComponent("v1")
+v2.RandomComponent("v2")
+v3.RandomComponent("v3")
+v4.RandomComponent("v4")
